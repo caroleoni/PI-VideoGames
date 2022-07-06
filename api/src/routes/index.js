9d4ca36e1ab7fe245a356d0a0be2108ae9d36507
caroleoni <carolinaleoni106@gmail.com>
2022-07-06 19:44:06 -0300

    deploy

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -9,8 +9,8 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.use('/routesVideoGames', routesVideoGames);
-router.use('/routesGenres', routesGenres);
+router.use('/videogames', routesVideoGames);
+router.use('/genres', routesGenres);
 
 
 module.exports = router;
